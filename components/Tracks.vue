68b6f4d98d2983786b639c858a96ca6d1a63e515
Jet-Green <grishadzyin@gmail.com>
2025-06-02 19:55:12 +0500

    add components

---
+++ components/Tracks.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+let tracks = ref([
+  {
+    title: "прикол1",
+    year: "2021",
+    artist_id: "Название исполнителя",
+    album_id: "Название альбома"
+  },
+  {
+    title: "прикол2",
+    year: "2021",
+    artist_id: "Название исполнителя",
+    album_id: "Название альбома"
+  },
+])
+</script>
+<template>
+  <v-card v-for="track of tracks">
+    {{ track.title }}
+    {{ track.year }}
+    {{ track.artist_id }}
+    {{ track.album_id }}
+  </v-card>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref } from "vue";
 
-const selectedTab = ref("Track");
+let selectedTab = ref("Track");
 
 function setTab(tab) {
   selectedTab.value = tab;
@@ -14,24 +14,34 @@ function setTab(tab) {
       <v-container class="text-center">
         <img src="~/assets/images/main-logo.svg" alt="">
         <v-row justify="center" class="controls">
-          <v-btn :color="selectedTab === 'Track' ? 'blue lighten-2' : 'white'"
-            :class="{ 'white--text': selectedTab === 'Track' }" class="mx-2" @click="setTab('Track')">
-            Track
-          </v-btn>
-          <v-btn :color="selectedTab === 'Album' ? 'blue lighten-2' : 'white'"
-            :class="{ 'white--text': selectedTab === 'Album' }" class="mx-2" @click="setTab('Album')">
-            Album
-          </v-btn>
-          <v-btn :color="selectedTab === 'Playlist' ? 'blue lighten-2' : 'white'"
-            :class="{ 'white--text': selectedTab === 'Playlist' }" class="mx-2" @click="setTab('Playlist')">
-            Playlist
-          </v-btn>
+          <v-col cols="12" sm="9" lg="6" xl="4" class="d-flex">
+            <v-btn :color="selectedTab === 'Track' ? 'blue lighten-2' : 'white'" width="30%"
+              :class="{ 'white--text': selectedTab === 'Track' }" class="mx-2" @click="setTab('Track')">
+              Трек
+            </v-btn>
+            <v-btn :color="selectedTab === 'Album' ? 'blue lighten-2' : 'white'" width="30%"
+              :class="{ 'white--text': selectedTab === 'Album' }" class="mx-2" @click="setTab('Album')">
+              Альбом
+            </v-btn>
+            <v-btn :color="selectedTab === 'Artist' ? 'blue lighten-2' : 'white'" width="30%"
+              :class="{ 'white--text': selectedTab === 'Artist' }" class="mx-2" @click="setTab('Artist')">
+              Исполнитель
+            </v-btn>
+          </v-col>
         </v-row>
 
         <v-row class="d-flex justify-center">
           <v-col cols="12" sm="9" lg="6" xl="4">
-            <v-text-field v-model="searchQuery" placeholder="Song 1" rounded variant="solo" class="search-input"
-              prepend-inner-icon="mdi-magnify"></v-text-field>
+            <v-text-field v-model="searchQuery" placeholder="Введите" rounded variant="solo" class="search-input"
+              append-inner-icon="mdi-magnify"></v-text-field>
+          </v-col>
+        </v-row>
+
+        <v-row class="d-flex justify-center">
+          <v-col cols="7">
+            <Tracks v-if="selectedTab == 'Track'" />
+            <Albums v-else-if="selectedTab == 'Album'" />
+            <Artists v-else-if="selectedTab == 'Artist'" />
           </v-col>
         </v-row>
       </v-container>
@@ -44,7 +54,7 @@ function setTab(tab) {
   background: linear-gradient(to bottom, #87ceeb, #1e90ff);
   height: 100vh;
   display: flex;
-  align-items: center;
+  // align-items: center;
   justify-content: center;
 }
 
