382f931238bad082a1cd716357a06ce60c0dd17a
kstasiie <amkopylova@edu.hse.ru>
2025-06-06 21:10:52 +0500

    trackCrad

--- components/TrackCard.vue
+++ components/TrackCard.vue
@@ -1,4 +1,12 @@
 <script setup lang="ts"></script>
 <template>
-  albums
+  <v-card class="mx-auto my-8" elevation="16" max-width="344">
+    <v-card-item>
+      <v-card-title> Track title </v-card-title>
+
+      <v-card-subtitle> Artist </v-card-subtitle>
+    </v-card-item>
+
+    <v-card-text> description </v-card-text>
+  </v-card>
 </template>
--- components/Tracks.vue
+++ components/Tracks.vue
@@ -4,21 +4,32 @@ let tracks = ref([
     title: "прикол1",
     year: "2021",
     artist_id: "Название исполнителя",
-    album_id: "Название альбома"
+    album_id: "Название альбома",
   },
   {
     title: "прикол2",
     year: "2021",
     artist_id: "Название исполнителя",
-    album_id: "Название альбома"
+    album_id: "Название альбома",
   },
-])
+]);
 </script>
 <template>
-  <v-card v-for="track of tracks">
-    {{ track.title }}
+  <v-card
+    v-for="track of tracks"
+    class="mx-auto my-8"
+    elevation="16"
+    max-width="300"
+    ><!--{{ track.title }}
     {{ track.year }}
     {{ track.artist_id }}
-    {{ track.album_id }}
+    {{ track.album_id }}-->
+    <v-card-item>
+      <v-card-title> {{ track.title }} </v-card-title>
+
+      <v-card-subtitle> {{ track.artist_id }} </v-card-subtitle>
+    </v-card-item>
+
+    <v-card-text> {{ track.year }} {{ track.album_id }} </v-card-text>
   </v-card>
 </template>
