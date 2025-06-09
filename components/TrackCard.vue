93c112a77ac761794dff3785adfc93b2a77c6a1b
kstasiie <amkopylova@edu.hse.ru>
2025-06-09 11:55:32 +0500

    NuxtLink + some buttons

--- components/TrackCard.vue
+++ components/TrackCard.vue
@@ -1,22 +1,26 @@
 <script setup lang="ts">
 defineProps<{
   track: {
-    title: string,
-    year: string,
-    artist_id: string,
-    album_id: string,
-  }
-}>()
-
+    title: string;
+    year: string;
+    artist_id: string;
+    album_id: string;
+  };
+}>();
 </script>
 <template>
   <v-card :elevation="0">
     <v-card-item>
       <v-card-title> {{ track.title }} </v-card-title>
 
-      <v-card-subtitle> {{ track.artist_id }} </v-card-subtitle>
+      <v-card-subtitle>
+        <NuxtLink to="/artist">{{ track.artist_id }}</NuxtLink>
+      </v-card-subtitle>
     </v-card-item>
 
-    <v-card-text> {{ track.year }} {{ track.album_id }} </v-card-text>
+    <v-card-text>
+      {{ track.year }}
+      <NuxtLink to="/album">{{ track.album_id }}</NuxtLink>
+    </v-card-text>
   </v-card>
 </template>
