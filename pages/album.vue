0f4f699bd0178737061b38f16fc1c93e03721c2d
kstasiie <amkopylova@edu.hse.ru>
2025-06-12 16:58:01 +0500

    add album page

--- pages/album.vue
+++ pages/album.vue
@@ -1,8 +1,45 @@
-<script setup lang="ts"></script>
+<script setup>
+let album = ref({
+  title: "Альбом1",
+  year: "2020",
+  artist_id: "artist_1",
+  genre_id: "rock",
+  tracks: [
+    {
+      title: "Song One",
+      year: "2020",
+      artist_id: "artist_1",
+      genre_id: "rock",
+    },
+    {
+      title: "Song Two",
+      year: "2020",
+      artist_id: "artist_1",
+      genre_id: "rock",
+    },
+  ],
+});
+</script>
 <template>
   <v-container class="text-center">
     <v-row justify="center" class="controls">
-      <v-col cols="12" sm="9" lg="6" xl="4" class="d-flex"> Album </v-col>
+      <v-col cols="12" sm="9" class="">
+        <v-card :elevation="0" class="mb-4">
+          <v-card-item>
+            <v-card-title>{{ album.title }}</v-card-title>
+            <v-card-subtitle> </v-card-subtitle>
+          </v-card-item>
+          <v-card-text>
+            <div>Released: {{ album.year }}</div>
+            <div>Genre: {{ album.genre_id }}</div>
+            <v-row>
+              <v-col cols="6" v-for="track in album.tracks" :key="track.title">
+                <TrackCard :track="track"></TrackCard>
+              </v-col>
+            </v-row>
+          </v-card-text>
+        </v-card>
+      </v-col>
     </v-row>
   </v-container>
 </template>
