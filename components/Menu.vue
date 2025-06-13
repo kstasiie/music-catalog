b305a4ca1f9371ed991d2439ceede7d79a19578b
kstasiie <amkopylova@edu.hse.ru>
2025-06-13 13:10:38 +0500

    add margin and navigation

--- components/Menu.vue
+++ components/Menu.vue
@@ -1,10 +1,14 @@
 <script setup>
-import { ref } from "vue";
-
 const open = ref(false);
 const fabLocation = ref("top right");
 const menuLocation = ref("bottom center");
 const transition = ref("scale-transition");
+
+const router = useRouter();
+
+function navigateTo(path) {
+  router.push(path);
+}
 </script>
 <template>
   <v-fab
@@ -13,6 +17,7 @@ const transition = ref("scale-transition");
     :location="fabLocation"
     size="large"
     icon
+    class="ma-3"
   >
     <v-icon>{{ open ? "mdi-close" : "mdi-crown" }}</v-icon>
     <v-speed-dial
@@ -21,19 +26,19 @@ const transition = ref("scale-transition");
       :transition="transition"
       activator="parent"
     >
-      <v-btn key="1" color="success" icon>
+      <v-btn key="1" color="success" icon @click="navigateTo('/adsfndf')">
         <v-icon size="24">mdi-check</v-icon>
       </v-btn>
 
-      <v-btn key="2" color="info" icon>
+      <v-btn key="2" color="info" icon @click="navigateTo('/adsfndf')">
         <v-icon size="24">mdi-information</v-icon>
       </v-btn>
 
-      <v-btn key="3" color="warning" icon>
+      <v-btn key="3" color="warning" icon @click="navigateTo('/adsfndf')">
         <v-icon size="24">mdi-alert</v-icon>
       </v-btn>
-
-      <v-btn key="4" color="error" icon>
+      <!-- кнопка, которая закрывает speed dial -->
+      <v-btn key="4" color="error" icon @click="open = false">
         <v-icon size="24">mdi-close-circle</v-icon>
       </v-btn>
     </v-speed-dial>
